c2a5e1430ad7cbce13412315d2edb5b9dc8e1a9a
chris <percist@gmail.com>
2020-10-01 18:57:07 -0400

    x,o, event listener

---
+++ tic-tac-toe.js
@@ -0,0 +1,27 @@
+const PLAYER_ONE_SYMBOL = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg";
+const PLAYER_TWO_SYMBOL = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    document
+        .getElementById("tic-tac-toe-board")
+        .addEventListener("click", (event) => {
+            console.log("IT WORKS!!")
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
