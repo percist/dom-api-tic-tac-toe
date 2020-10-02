3f5dd8e48dcfbb3eacf50f2f8cfcd5d6f622853c
chris <percist@gmail.com>
2020-10-02 16:52:29 -0400

    begin status check

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,6 +7,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
   let currentPlayerSymbol = "x";
   let squareValues = ["", "", "", "", "", "", "", "", ""];
   let playerMove;
+  let statusOfGame = "";
+
+  let checkStatusOfGame = () =>{
+    //Winning combinations: [[0,3,6}, [1,4,7], [2,5,8], [0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6]]
+    for(let i = 0; i < 9; i += 3){
+      if(squareValues[i] !== '' && squareValues[i] === squareValues[i+1] && squareValues[i] === squareValues[i+2]){
+
+      }
+    }
+  }
 
   document
     .getElementById("tic-tac-toe-board")
