e689aeeed7540fef6db3de7dae3a39147b4e0fc5
Jesse <jbenlind@gmail.com>
2020-10-02 12:39:43 -0500

    phase two, creating new function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,15 +2,23 @@ const PLAYER_ONE_SYMBOL = "https://assets.aaonline.io/Module-DOM-API/formative-p
 const PLAYER_TWO_SYMBOL = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
 
 window.addEventListener("DOMContentLoaded", (event) => {
+    const currentPlayerSymbol = 'x';
+    const squareValues = ['', '', '', '', '', '', '', '', ''];
+    let playerMove =
 
-    document
-        .getElementById("tic-tac-toe-board")
-        .addEventListener("click", (event) => {
-            console.log("IT WORKS!!")
+    document.getElementById("tic-tac-toe-board").addEventListener("click", (event) => {
+        const targetEvent = event.target.id;
 
-        });
-    const showOrHide = () => {
-        let squares = document.querySelectorAll("#")
+        if(!targetEvent.startsWith('square-')) {
+            return;
+        } else {
+           playerMove = Number.parseInt(targetEvent[targetEvent.length -1]);
+        }
+
+
+    });
+    const occupiedSpace = () => {
+
     }
 
 
